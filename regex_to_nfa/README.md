0ef48dfe720504f802a13f1a5a0532a0cee1251a
Sofia279 <sofialatorre313@ciencias.unam.mx>
2026-02-17 10:55:13 -0600

    docs: add to basic directory structre

---
+++ regex_to_nfa/README.md
@@ -0,0 +1,11 @@
+# Practical Assignment I: From Regular Expressions to NFA
+
+Implementation of
+
+## Structure
+- `src/main.c`
+
+## Compilation and Execution
+
+## Examples
+
--- regex_to_nfa/src/main.c
+++ regex_to_nfa/src/main.c
@@ -1,27 +1,23 @@
-#include "regex.h"
-#include "nfa.h"
+#include "regex.h"      // Regular expression
+#include "nfa.h"        // Nondeterministic Finite Automaton (NFA)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
 
-void print_postfix(regex r)
-{
-    for (int i = 0; i < r.size; i++)
-    {
+void print_postfix(regex r){
+    for (int i = 0; i < r.size; i++) {
         printf("%c", r.items[i].value);
     }
     printf("\n");
 }
 
-void test_strings_stdin(const char *regex_str)
-{
+void test_strings_stdin(const char *regex_str) {
     regex r = parse_regex(regex_str);
     nfa n = regex_to_nfa(r);
 
     char buf[1024];
-    while (fgets(buf, sizeof(buf), stdin))
-    {
+    while (fgets(buf, sizeof(buf), stdin)) {
         buf[strcspn(buf, "\r\n")] = '\0';
         int result = match_nfa(n, buf, strlen(buf));
         printf("%d", result ? 1 : 0);
@@ -29,15 +25,12 @@ void test_strings_stdin(const char *regex_str)
     printf("\n");
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int opt;
     char regex_str[1024];
 
-    while ((opt = getopt(argc, argv, "rt")) != -1)
-    {
-        switch (opt)
-        {
+    while ((opt = getopt(argc, argv, "rt")) != -1) {
+        switch (opt){
             case 'r':
                 if (!fgets(regex_str, sizeof(regex_str), stdin))
                     return 1;
